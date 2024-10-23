4194c755d07074dfff933232da738a1ddea066a0
Marcos Vidal González <marcos.vidal76@gmail.com>
2024-10-23 20:38:18 +0200

    Almacen central falta implementacion monedas en comida

--- src/commons/AlmacenCentral.java
+++ src/commons/AlmacenCentral.java
@@ -1,5 +1,103 @@
 package commons;
 
+// CLASE QUE REPRESENTA UN ALMACEN CENTRAL
 public class AlmacenCentral {
 
+    // ATRIBUTOS
+    private int comidaVegetal;
+    private int comidaAnimal;
+    private final int capacidadMaxVeg = 200;
+    private final int capacidadMaxAni = 200;
+
+    // CONSTRUCTOR
+    public AlmacenCentral() {
+        this.comidaVegetal = 0;
+        this.comidaAnimal = 0;
+    }
+
+    // GETTERS Y SETTERS
+    /**
+     * @return int return the comidaVegetal
+     */
+    public int getComidaVegetal() {
+        return comidaVegetal;
+    }
+
+    /**
+     * @param comidaVegetal the comidaVegetal to set
+     */
+    public void setComidaVegetal(int comidaVegetal) {
+        this.comidaVegetal = comidaVegetal;
+    }
+
+    /**
+     * @return int return the comidaAnimal
+     */
+    public int getComidaAnimal() {
+        return comidaAnimal;
+    }
+
+    /**
+     * @param comidaAnimal the comidaAnimal to set
+     */
+    public void setComidaAnimal(int comidaAnimal) {
+        this.comidaAnimal = comidaAnimal;
+    }
+
+
+    // METODOS DE LA CLASE
+
+    public void agregarComidaVeg(int cantidad) { //TODO IMPLEMENTAR SISTEMA MONEDAS
+        if (cantidad < 0 ) {
+            System.out.println("No es posible añadir una cantidad de comida negativa");
+            return;
+        }
+        comidaVegetal += cantidad;
+        if (comidaVegetal > capacidadMaxVeg
+        ) {
+            comidaVegetal = capacidadMaxVeg;
+            System.out.println("Capacidad máxima alcanzada para la comida vegetal.");
+        }
+    }
+
+    
+    public void agregarComidaAnimal(int cantidad) {
+        if (cantidad < 0) {
+            System.out.println("No se puede agregar una cantidad negativa.");
+            return;
+        }
+
+        comidaAnimal += cantidad;
+        if (comidaAnimal > capacidadMaxAni) {
+            comidaAnimal = capacidadMaxAni;
+            System.out.println("Capacidad máxima alcanzada para la comida animal.");
+        }
+    }
+
+
+     // Método para distribuir comida a las piscifactorías
+     public void distribuirComida(int cantidadVegetal, int cantidadAnimal, int numPiscifactorías) {
+        int totalNecesitadoVegetal = cantidadVegetal * numPiscifactorías;
+        int totalNecesitadoAnimal = cantidadAnimal * numPiscifactorías;
+
+        if (totalNecesitadoVegetal <= comidaVegetal) {
+            comidaVegetal -= totalNecesitadoVegetal;
+            System.out.println("Distribuidos " + totalNecesitadoVegetal + " de comida vegetal.");
+        } else {
+            System.out.println("No hay suficiente comida vegetal para distribuir.");
+        }
+
+        if (totalNecesitadoAnimal <= comidaAnimal) {
+            comidaAnimal -= totalNecesitadoAnimal;
+            System.out.println("Distribuidos " + totalNecesitadoAnimal + " de comida animal.");
+        } else {
+            System.out.println("No hay suficiente comida animal para distribuir.");
+        }
+    }
+
+    public void mostrarEstado() {
+        System.out.println("Estado del Almacén Central:");
+        System.out.println("Comida Vegetal: " + comidaVegetal + " / " + capacidadMaxVeg);
+        System.out.println("Comida Animal: " + comidaAnimal + " / " + capacidadMaxAni);
+    }
 }
--- src/commons/Simulador.java
+++ src/commons/Simulador.java
@@ -2,4 +2,21 @@ package commons;
 
 public class Simulador {
 
-}
+    public static void main(String[] args) {
+        AlmacenCentral almacen = new AlmacenCentral();
+        
+        almacen.agregarComidaVeg(150);
+        almacen.agregarComidaAnimal(180);
+        
+        almacen.mostrarEstado();
+        
+        almacen.distribuirComida(50, 30, 2); // Distribuye comida para 2 piscifactorías.
+        
+        almacen.mostrarEstado();
+        
+        // Usando getters
+        System.out.println("Cantidad de comida vegetal actual: " + almacen.getComidaVegetal());
+        System.out.println("Cantidad de comida animal actual: " + almacen.getComidaAnimal());
+        
+        }
+    }
