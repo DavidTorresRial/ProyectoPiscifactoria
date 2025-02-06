59bde422d23ace84fba5ea3280cd3c408df52d50
dtorres <davidtr05@icloud.com>
2025-02-06 18:03:02 +0100

    Correcciones en la clase Conexion que no iba ni para atras agrego la clase DAO y agrego la clase generador de Fran.

--- src/database/Conexion.java
+++ src/database/Conexion.java
@@ -7,22 +7,22 @@ import java.sql.SQLException;
 import commons.Simulador;
 
 /** Administra la conexión a una base de datos MySQL. */
-public class MySQLConnection {
+public class Conexion {
 
     /** Usuario de la base de datos. */
-    private static final String USER = " "; 
+    private static final String USER = "dtorres"; 
 
     /** Contraseña del usuario de la base de datos. */
-    private static final String PASSWORD = " ";
+    private static final String PASSWORD = "654045784#";
 
     /** Dirección del servidor de la base de datos. */
-    private static final String SERVER = " .iescotarelo.es";
+    private static final String SERVER = "dtorres.iescotarelo.es";
 
     /** Puerto del servidor de la base de datos. */
-    private static final String PORT = " ";
+    private static final String PORT = "2005";
 
     /** Nombre de la base de datos. */
-    private static final String DATABASE = " ";
+    private static final String DATABASE = "pisci";
 
     /** Objeto de conexión a la base de datos. */
     private static Connection connection;
@@ -34,8 +34,8 @@ public class MySQLConnection {
     public static Connection getConnection() {
         if (connection == null) {
             try {
-                connection = DriverManager.getConnection("jdbc:mysql://" + SERVER + ":" + PORT + "/" + DATABASE, USER, PASSWORD);
-                // System.out.println("Conexión exitosa.");
+                connection = DriverManager.getConnection("jdbc:mysql://" + SERVER + ":" + PORT + "/" + DATABASE + "?rewriteBatchedStatements=true", USER, PASSWORD);
+                System.out.println("Conexión exitosa.");
             } catch (SQLException e) {
                 e.printStackTrace();
                 Simulador.registro.registroLogError("Error al conectar: " + e.getMessage());
@@ -50,21 +50,29 @@ public class MySQLConnection {
             try {
                 connection.close();
                 connection = null;
-                // System.out.println("Conexión cerrada.");
+                System.out.println("Conexión cerrada.");
             } catch (SQLException e) {
                 Simulador.registro.registroLogError("Error al cerrar conexión: " + e.getMessage());
             }
         }
     }
 
-    /*
+    
 
 
     public static void main(String[] args) {
-    connection = MySQLConnection.getConnection(); // TODO Pruebas
-    MySQLConnection.closeConnection();
-    }
+        connection = Conexion.getConnection(); // TODO Pruebas
 
-    
-    */
+        GeneradorBD.crearTablaCliente();
+        GeneradorBD.crearTablaPez();
+        GeneradorBD.crearTablaPedido();
+        //GeneradorBD.agregarClientes();
+
+        
+        DAOPedidos pedidos = new DAOPedidos();
+        pedidos.enviarPedido("PED1738860948903836", 102345432);
+        pedidos.generarPedidoAutomatico();
+
+        Conexion.closeConnection();        
+    }
 }
